a29abf847c0a45c7268558e976bc926e69f6cff0
TakeFives <shturvalova.m@gmail.com>
2022-11-25 22:52:09 +0200

    feat: create and style slides component

--- src/app/features/home/home.module.ts
+++ src/app/features/home/home.module.ts
@@ -2,9 +2,13 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from "@angular/router";
 import { HomeComponent } from './home.component';
+import { SlideComponent } from './slide/slide.component';
 
 @NgModule({
-  declarations: [HomeComponent],
+  declarations: [
+    HomeComponent,
+    SlideComponent
+  ],
   imports: [
     CommonModule,
     RouterModule.forChild([{ path: '', component: HomeComponent }])
