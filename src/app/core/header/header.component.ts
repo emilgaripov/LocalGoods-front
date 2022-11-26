9c09717e00eedcfa94653ceae5ddf12a850665ad
TakeFives <shturvalova.m@gmail.com>
2022-11-27 01:57:20 +0200

    style: add styles to header nav and toggle

--- src/app/core/header/header.component.ts
+++ src/app/core/header/header.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, Renderer2, ViewChild } from '@angular/core';
 import { NavigationStart, Router } from "@angular/router";
 
 @Component({
@@ -7,10 +7,24 @@ import { NavigationStart, Router } from "@angular/router";
   styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
+  @ViewChild('toggleButton') toggleButton!: ElementRef;
+  @ViewChild('nav') menu!: ElementRef
+
   currentPage!: string;
   term = '';
+  nav = false;
 
-  constructor(private router: Router) { }
+  constructor(
+    private router: Router,
+    private renderer: Renderer2
+  ) {
+    this.renderer.listen('window', 'click', (e: Event) => {
+      if (!this.nav) return
+      if (e.target !== this.toggleButton.nativeElement && !this.menu.nativeElement.contains(e.target)) {
+        this.nav = false;
+      }
+    });
+  }
 
   ngOnInit(): void {
     this.router.events.subscribe({
@@ -20,6 +34,12 @@ export class HeaderComponent implements OnInit {
         }
       }
     });
+
+  }
+
+  toggleMenu(event: Event) {
+    event.stopImmediatePropagation();
+    this.nav = !this.nav
   }
 
 }
