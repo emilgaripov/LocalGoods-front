6f2713b404637f7bdfa38f928248138954e415b8
TakeFives <shturvalova.m@gmail.com>
2022-11-29 16:16:05 +0200

    style: edit footer style and flex grow

--- src/app/features/products/products.component.ts
+++ src/app/features/products/products.component.ts
@@ -3,7 +3,10 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-products',
   templateUrl: './products.component.html',
-  styleUrls: ['./products.component.scss']
+  styleUrls: ['./products.component.scss'],
+  host: {
+    class: 'grow-container'
+  }
 })
 export class ProductsComponent implements OnInit {
 
