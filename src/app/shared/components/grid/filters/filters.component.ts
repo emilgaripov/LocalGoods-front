b164d9cce0a0f694911aad0e4bc39a64a689b7fe
TakeFives <shturvalova.m@gmail.com>
2022-12-01 11:06:49 +0200

    feat: add filter component to grid

---
+++ src/app/shared/components/grid/filters/filters.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-filters',
+  templateUrl: './filters.component.html',
+  styleUrls: ['./filters.component.scss']
+})
+export class FiltersComponent implements OnInit {
+
+  showChild: boolean = false;
+  
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
