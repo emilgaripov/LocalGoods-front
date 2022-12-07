cde1bde3931d38539868e495ed51b12ee9842ef6
TakeFives <shturvalova.m@gmail.com>
2022-12-07 17:32:56 +0200

    feat: add validators to auth form

--- src/app/features/auth/auth.component.ts
+++ src/app/features/auth/auth.component.ts
@@ -7,20 +7,31 @@ type RegistrationFormValues = { email: string, password: string, username: strin
   selector: 'app-auth',
   templateUrl: './auth.component.html',
   styleUrls: ['./auth.component.scss'],
+  host: {
+    class: 'grow-container'
+  },
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class AuthComponent {
   loginMode = true;
+  submitted: boolean = false;
 
   switchMode() {
     this.loginMode = !this.loginMode;
   }
 
   onLogin(formValues: LoginFormValues) {
+    this.submitted = true // to aviod repeated submit click
     console.log(formValues)
+    this.resetForm()
   }
 
   onRegister(formValues: RegistrationFormValues) {
     console.log(formValues)
+    this.resetForm()
+  }
+
+  resetForm(){
+    this.submitted = false
   }
 }
