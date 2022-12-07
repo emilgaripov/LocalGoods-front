d1b28d4e58f9c716461e4b37b38c46d908f0a30c
TakeFives <shturvalova.m@gmail.com>
2022-12-08 01:20:46 +0200

    feat: add validators to farm creation form

--- src/app/features/farmer/farmer-farms/farmer-farms.component.ts
+++ src/app/features/farmer/farmer-farms/farmer-farms.component.ts
@@ -13,15 +13,19 @@ import { UserService } from "../../../shared/services/user.service";
 export class FarmerFarmsComponent implements OnInit {
   farms$!: Observable<IFarm[]>;
   isModalOpened = false;
+  submitted = false;
 
   //for map default cords of Zurich
   latitude = 47.373878;
   longitude = 8.545094;
 
+  //image upload
+  fileName = '';
+
   constructor(
     private farmsService: FarmsService,
     private userService: UserService
-  ) {}
+  ) { }
 
   ngOnInit(): void {
     const userId = this.userService.currentUser.id;
@@ -37,15 +41,28 @@ export class FarmerFarmsComponent implements OnInit {
   }
 
   onAddFarm(value: any) {
+    this.submitted = true;
+
+
     if (!value.farmName) return;
 
     // this.farmerFarmsService.createFarm(value.farmName);
+    this.submitted = false; // to add to succes response
     this.closeModal();
   }
 
-  onChoseLocation(event:any){
+  onChoseLocation(event: any) {
     console.log(event);
     this.latitude = event.coords.lat;
     this.longitude = event.coords.lng;
+  }
+
+  onFileSelected(event: any) {
+    console.log('event.target');
+    const file: File = event.target.files[0];
+
+    if (file) {
+      this.fileName = file.name;
+    }
   }
 }
