51d4c0b3397fa58032781fcfb66cd78b379b1702
TakeFives <shturvalova.m@gmail.com>
2022-12-12 16:54:10 +0200

    refactor: edit data routes to display on map

--- src/app/features/home/map/map.component.ts
+++ src/app/features/home/map/map.component.ts
@@ -1,4 +1,7 @@
-import { ChangeDetectionStrategy, Component } from '@angular/core';
+import { ChangeDetectionStrategy, Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { IFarm } from 'src/app/shared/interfaces/farm.interface';
+import { FarmsService } from 'src/app/shared/services/farms.service';
 
 @Component({
   selector: 'app-map',
@@ -6,35 +9,46 @@ import { ChangeDetectionStrategy, Component } from '@angular/core';
   styleUrls: ['./map.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class MapComponent {
+export class MapComponent implements OnInit {
   zoom: number = 8;
   lat: number = 51.678418;
   lng: number = 7.809007;
 
-  markers = [
-    {
-      id: 1,
-      name: 'Farm 1',
-      image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
-      lat: 53.551086,
-      lng: 9.993682
-    },
-    {
-      id: 2,
-      name: 'Farm 2',
-      image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
-      lat: 52.520008,
-      lng: 13.404954
-    },
-    {
-      id: 3,
-      name: 'Farm 3',
-      image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
-      lat: 51.050407,
-      lng: 13.737262
-    }
-  ]
+  farms$!: Observable<IFarm[]>
 
+  constructor(
+    private farmsService: FarmsService
+  ){
+
+  }
+
+  // markers = [
+  //   {
+  //     id: 1,
+  //     name: 'Farm 1',
+  //     image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
+  //     lat: 53.551086,
+  //     lng: 9.993682
+  //   },
+  //   {
+  //     id: 2,
+  //     name: 'Farm 2',
+  //     image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
+  //     lat: 52.520008,
+  //     lng: 13.404954
+  //   },
+  //   {
+  //     id: 3,
+  //     name: 'Farm 3',
+  //     image: 'https://live.staticflickr.com/65535/50881797506_176f3d534f_z.jpg',
+  //     lat: 51.050407,
+  //     lng: 13.737262
+  //   }
+  // ]
+
+  ngOnInit(){
+    this.farms$ = this.farmsService.getAllFarms()
+  }
 
   clickedMarker(name: string, index: number){
     console.log('marker name', name, 'index', index);
