d7bbedb16eb9bdfa6184ed55b52cb2a2f111d56e
TakeFives <shturvalova.m@gmail.com>
2022-12-14 16:48:25 +0200

    fix: connect tap from lib

--- src/app/shared/services/user.service.ts
+++ src/app/shared/services/user.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient, HttpErrorResponse } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { catchError, throwError } from 'rxjs';
+import { catchError, tap, throwError } from 'rxjs';
 import { environment } from 'src/environments/environment';
 import { IUser } from '../interfaces/user.interface';
 import { editUserFormData } from '../types/types';
